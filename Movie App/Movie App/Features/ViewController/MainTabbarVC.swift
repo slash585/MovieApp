ebf53efc2cc6d83c4237f333cd288cd2055d899c
slash585 <slsh585@gmail.com>
2023-08-24 11:23:55 +0300

    core: remove storyboard and init app

---
+++ Movie App/Movie App/Features/ViewController/MainTabbarVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie App
+//
+//  Created by Mehmet Ali Özdemir on 24.08.2023.
+//
+
+import UIKit
+
+class MainTabbarVC: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
